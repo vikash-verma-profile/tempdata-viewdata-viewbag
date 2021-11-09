06f9b58826d82670398efd4d817f5efd83f010af
vikash-verma-profile <er.vikashverma@yahoo.com>
2021-11-09 09:33:29 +0400

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            dynamic i = 1;
+            i = "Vikash";
+            i = 1.57;
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+
+            //View Data
+
+
+
+            List<string> student = new List<string>();
+            student.Add("Vikash");
+            student.Add("Sumit");
+            student.Add("Amit");
+            //ViewData["Students"] = student;
+            //ViewData["Message"] = "I am a message";
+
+
+            //view Bag
+            //ViewBag.Students = student;
+
+            TempData["Students"] = student;
+            Session["messgae"] = "Hi i am session";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Submit()
+        {
+
+            var sample = TempData["Students"];
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
